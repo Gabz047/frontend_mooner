4f6d01ffe2a2450d2392fe51b4b115344f873436
luansilva25 <luansilva250807@gmail.com>
2024-10-04 22:41:44 -0300

    login complete system

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,48 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+const token = localStorage.getItem('access')
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta:{
+        auth: true
+      }
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/auth/LoginView.vue')
+      component: () => import('../views/auth/LoginView.vue'),
+      meta:{
+        auth: false
+      }
     },
     {
       path: '/sign-up',
       name: 'sign-up',
-      component: () => import('../views/auth/SignView.vue')
+      component: () => import('../views/auth/SignView.vue'),
+      meta:{
+        auth: false
+      }
     },
     {
       path: '/artist/:id',
-      name: 'artist',
-      component: () => import('../views/ArtistDetailView.vue')
+      name: 'sign-up',
+      component: () => import('../views/ArtistDetailView.vue'),
+      meta:{
+        auth: false
+      }
+    },
+    { 
+      path: '/:pathMatch(.*)*', 
+      name: 'NotFound', 
+      component: ()=> import('../views/NotFound.vue')
     }
   ]
 })
-
 export default router
