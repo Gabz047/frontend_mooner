6a03470ee29757e5d2cc955d4ab097b65309bc4d
Gabz047 <gabe1612mail@gmail.com>
2024-10-09 11:36:52 -0300

    FEAT: Created global music container #21

---
+++ src/views/Test.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MusicGlobalContainer from '@/components/global/MusicGlobalContainer.vue';
+import MusicBox from '@/components/global/MusicBox.vue';
+import { data_music } from '@/utils/music/music';
+</script>
+<template>
+<main class="w-dvw h-dvh bg-black">
+    <MusicGlobalContainer >
+        <MusicBox v-for="item, index in data_music" :key="index" :music_data="item" :index="index" @createPlaylist="item.has_playlist = !item.has_playlist" :has_playlist="item.has_playlist" />
+    </MusicGlobalContainer>
+</main>
+</template>
