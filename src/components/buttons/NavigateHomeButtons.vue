89efd7c7dff56eae0d704a49e732f87781f1f1dc
Gabz047 <gabe1612mail@gmail.com>
2024-10-14 10:21:35 -0300

    FEAT: Created Navigate Home Button component #19

---
+++ src/components/buttons/NavigateHomeButtons.vue
@@ -0,0 +1,15 @@
+<script>
+const emits = defineEmits([
+    'goSection'
+])
+
+const props = defineProps({
+    active: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
+<template>
+    <span @click="emits('goSection')" :class="props.active ? 'font-bold' : 'font-normal'" class="px-5 py-2 text-white bg-[#242424] rounded-xl">Músicas</span>
+</template>
