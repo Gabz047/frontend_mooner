e0a9635e5ff94d8f610366b3ddfcf53e3148466a
Gabz047 <gabe1612mail@gmail.com>
2024-12-07 04:36:26 -0300

    FEAT: Created New Global Header

---
+++ src/components/global/headerGlobal/GlobalHeader.vue
@@ -0,0 +1,24 @@
+<script setup>
+import SearchBar from './SearchBar.vue';
+import { useUserStore } from '@/stores';
+import { UserPerfil } from '@/components';
+import { shallowRef } from 'vue';
+const userStore = useUserStore()
+
+</script>
+
+<template>
+    <section class="w-full flex justify-between h-[47px] items-center">
+      <SearchBar />
+      <div class="flex text-white gap-2 mr-2">
+        <UserPerfil :data="userStore.myuser" />
+        
+        <div class="flex gap-2 lg:hidden">
+        <span class="mdi mdi-account-multiple text-2xl rounded-full h-[50px] w-[50px] bg-[#151515] flex items-center justify-center"></span>
+        <span class="mdi mdi-cog text-2xl rounded-full h-[50px] w-[50px] bg-[#151515] flex items-center justify-center"></span>
+        </div>
+      </div>
+      
+    </section>
+
+</template>
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import GlobalHeader from "./global/headerGlobal/GlobalHeader.vue";
+import SideHeader from "./newDesign/SideHeader.vue";
+import SearchBar from "./global/headerGlobal/SearchBar.vue";
+import UserPerfil from "./global/headerGlobal/UserPerfil.vue";
+import SearchOptions from "./global/headerGlobal/SearchOptions.vue";
+import MusicBoxExtended from "./music/MusicBoxExtended.vue";
+export {
+    GlobalHeader,
+    SideHeader,
+    SearchBar,
+    UserPerfil,
+    SearchOptions,
+    MusicBoxExtended
+}
